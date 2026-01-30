6d32dc78041ebab9a1027622085b4a60aaef19d6
Poligonais <cgpg@mpor.gov.br>
2026-01-30 09:20:06 -0300

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,92 +1,93 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:4326").setExtent([-52.347441, -31.832872, -52.177738, -31.742378]);
 var wms_layers = [];
 
 
         var lyr_GoogleSatellite_0 = new ol.layer.Tile({
             'title': 'Google Satellite',
-            //'type': 'base',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
-    attributions: ' ',
+            attributions: ' ',
                 url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
-var format_AnexoIVreaAquaviria_1 = new ol.format.GeoJSON();
-var features_AnexoIVreaAquaviria_1 = format_AnexoIVreaAquaviria_1.readFeatures(json_AnexoIVreaAquaviria_1, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_AnexoIVreaAquaviria_1 = new ol.source.Vector({
+var format_Anexo04reaAquaviria_1 = new ol.format.GeoJSON();
+var features_Anexo04reaAquaviria_1 = format_Anexo04reaAquaviria_1.readFeatures(json_Anexo04reaAquaviria_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Anexo04reaAquaviria_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_AnexoIVreaAquaviria_1.addFeatures(features_AnexoIVreaAquaviria_1);
-var lyr_AnexoIVreaAquaviria_1 = new ol.layer.Vector({
+jsonSource_Anexo04reaAquaviria_1.addFeatures(features_Anexo04reaAquaviria_1);
+var lyr_Anexo04reaAquaviria_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_AnexoIVreaAquaviria_1, 
-                style: style_AnexoIVreaAquaviria_1,
-                popuplayertitle: "Anexo IV - Área Aquaviária",
+                source:jsonSource_Anexo04reaAquaviria_1, 
+                style: style_Anexo04reaAquaviria_1,
+                popuplayertitle: 'Anexo 04 - Área Aquaviária',
                 interactive: true,
-                title: '<img src="styles/legend/AnexoIVreaAquaviria_1.png" /> Anexo IV - Área Aquaviária'
+                title: '<img src="styles/legend/Anexo04reaAquaviria_1.png" /> Anexo 04 - Área Aquaviária'
             });
-var format_AnexoIIIreadeExpansodoPorto_2 = new ol.format.GeoJSON();
-var features_AnexoIIIreadeExpansodoPorto_2 = format_AnexoIIIreadeExpansodoPorto_2.readFeatures(json_AnexoIIIreadeExpansodoPorto_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_AnexoIIIreadeExpansodoPorto_2 = new ol.source.Vector({
+var format_Anexo03readeExpansodoPorto_2 = new ol.format.GeoJSON();
+var features_Anexo03readeExpansodoPorto_2 = format_Anexo03readeExpansodoPorto_2.readFeatures(json_Anexo03readeExpansodoPorto_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Anexo03readeExpansodoPorto_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_AnexoIIIreadeExpansodoPorto_2.addFeatures(features_AnexoIIIreadeExpansodoPorto_2);
-var lyr_AnexoIIIreadeExpansodoPorto_2 = new ol.layer.Vector({
+jsonSource_Anexo03readeExpansodoPorto_2.addFeatures(features_Anexo03readeExpansodoPorto_2);
+var lyr_Anexo03readeExpansodoPorto_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_AnexoIIIreadeExpansodoPorto_2, 
-                style: style_AnexoIIIreadeExpansodoPorto_2,
-                popuplayertitle: "Anexo III - Área de Expansão do Porto",
+                source:jsonSource_Anexo03readeExpansodoPorto_2, 
+                style: style_Anexo03readeExpansodoPorto_2,
+                popuplayertitle: 'Anexo 03 - Área de Expansão do Porto',
                 interactive: true,
-                title: '<img src="styles/legend/AnexoIIIreadeExpansodoPorto_2.png" /> Anexo III - Área de Expansão do Porto'
+                title: '<img src="styles/legend/Anexo03readeExpansodoPorto_2.png" /> Anexo 03 - Área de Expansão do Porto'
             });
-var format_AnexoIIreadeDocas_3 = new ol.format.GeoJSON();
-var features_AnexoIIreadeDocas_3 = format_AnexoIIreadeDocas_3.readFeatures(json_AnexoIIreadeDocas_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_AnexoIIreadeDocas_3 = new ol.source.Vector({
+var format_Anexo02readeDocas_3 = new ol.format.GeoJSON();
+var features_Anexo02readeDocas_3 = format_Anexo02readeDocas_3.readFeatures(json_Anexo02readeDocas_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Anexo02readeDocas_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_AnexoIIreadeDocas_3.addFeatures(features_AnexoIIreadeDocas_3);
-var lyr_AnexoIIreadeDocas_3 = new ol.layer.Vector({
+jsonSource_Anexo02readeDocas_3.addFeatures(features_Anexo02readeDocas_3);
+var lyr_Anexo02readeDocas_3 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_AnexoIIreadeDocas_3, 
-                style: style_AnexoIIreadeDocas_3,
-                popuplayertitle: "Anexo II - Área de Docas",
+                source:jsonSource_Anexo02readeDocas_3, 
+                style: style_Anexo02readeDocas_3,
+                popuplayertitle: 'Anexo 02 - Área de Docas',
                 interactive: true,
-                title: '<img src="styles/legend/AnexoIIreadeDocas_3.png" /> Anexo II - Área de Docas'
+                title: '<img src="styles/legend/Anexo02readeDocas_3.png" /> Anexo 02 - Área de Docas'
             });
-var format_AnexoIreaPrincipaldoPorto_4 = new ol.format.GeoJSON();
-var features_AnexoIreaPrincipaldoPorto_4 = format_AnexoIreaPrincipaldoPorto_4.readFeatures(json_AnexoIreaPrincipaldoPorto_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_AnexoIreaPrincipaldoPorto_4 = new ol.source.Vector({
+var format_Anexo01reaPrincipaldoPorto_4 = new ol.format.GeoJSON();
+var features_Anexo01reaPrincipaldoPorto_4 = format_Anexo01reaPrincipaldoPorto_4.readFeatures(json_Anexo01reaPrincipaldoPorto_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Anexo01reaPrincipaldoPorto_4 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_AnexoIreaPrincipaldoPorto_4.addFeatures(features_AnexoIreaPrincipaldoPorto_4);
-var lyr_AnexoIreaPrincipaldoPorto_4 = new ol.layer.Vector({
+jsonSource_Anexo01reaPrincipaldoPorto_4.addFeatures(features_Anexo01reaPrincipaldoPorto_4);
+var lyr_Anexo01reaPrincipaldoPorto_4 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_AnexoIreaPrincipaldoPorto_4, 
-                style: style_AnexoIreaPrincipaldoPorto_4,
-                popuplayertitle: "Anexo I - Área Principal do Porto",
+                source:jsonSource_Anexo01reaPrincipaldoPorto_4, 
+                style: style_Anexo01reaPrincipaldoPorto_4,
+                popuplayertitle: 'Anexo 01 - Área Principal do Porto',
                 interactive: true,
-                title: '<img src="styles/legend/AnexoIreaPrincipaldoPorto_4.png" /> Anexo I - Área Principal do Porto'
+                title: '<img src="styles/legend/Anexo01reaPrincipaldoPorto_4.png" /> Anexo 01 - Área Principal do Porto'
             });
 
-lyr_GoogleSatellite_0.setVisible(true);lyr_AnexoIVreaAquaviria_1.setVisible(true);lyr_AnexoIIIreadeExpansodoPorto_2.setVisible(true);lyr_AnexoIIreadeDocas_3.setVisible(true);lyr_AnexoIreaPrincipaldoPorto_4.setVisible(true);
-var layersList = [lyr_GoogleSatellite_0,lyr_AnexoIVreaAquaviria_1,lyr_AnexoIIIreadeExpansodoPorto_2,lyr_AnexoIIreadeDocas_3,lyr_AnexoIreaPrincipaldoPorto_4];
-lyr_AnexoIVreaAquaviria_1.set('fieldAliases', {'Name': 'Name', });
-lyr_AnexoIIIreadeExpansodoPorto_2.set('fieldAliases', {'Name': 'Name', });
-lyr_AnexoIIreadeDocas_3.set('fieldAliases', {'Name': 'Name', });
-lyr_AnexoIreaPrincipaldoPorto_4.set('fieldAliases', {'Name': 'Name', });
-lyr_AnexoIVreaAquaviria_1.set('fieldImages', {'Name': 'TextEdit', });
-lyr_AnexoIIIreadeExpansodoPorto_2.set('fieldImages', {'Name': 'TextEdit', });
-lyr_AnexoIIreadeDocas_3.set('fieldImages', {'Name': 'TextEdit', });
-lyr_AnexoIreaPrincipaldoPorto_4.set('fieldImages', {'Name': 'TextEdit', });
-lyr_AnexoIVreaAquaviria_1.set('fieldLabels', {'Name': 'inline label - always visible', });
-lyr_AnexoIIIreadeExpansodoPorto_2.set('fieldLabels', {'Name': 'inline label - always visible', });
-lyr_AnexoIIreadeDocas_3.set('fieldLabels', {'Name': 'inline label - always visible', });
-lyr_AnexoIreaPrincipaldoPorto_4.set('fieldLabels', {'Name': 'inline label - always visible', });
-lyr_AnexoIreaPrincipaldoPorto_4.on('precompose', function(evt) {
+lyr_GoogleSatellite_0.setVisible(true);lyr_Anexo04reaAquaviria_1.setVisible(true);lyr_Anexo03readeExpansodoPorto_2.setVisible(true);lyr_Anexo02readeDocas_3.setVisible(true);lyr_Anexo01reaPrincipaldoPorto_4.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_Anexo04reaAquaviria_1,lyr_Anexo03readeExpansodoPorto_2,lyr_Anexo02readeDocas_3,lyr_Anexo01reaPrincipaldoPorto_4];
+lyr_Anexo04reaAquaviria_1.set('fieldAliases', {'Name': 'Nome', });
+lyr_Anexo03readeExpansodoPorto_2.set('fieldAliases', {'Name': 'Nome', });
+lyr_Anexo02readeDocas_3.set('fieldAliases', {'Name': 'Name', });
+lyr_Anexo01reaPrincipaldoPorto_4.set('fieldAliases', {'Name': 'Nome', });
+lyr_Anexo04reaAquaviria_1.set('fieldImages', {'Name': 'TextEdit', });
+lyr_Anexo03readeExpansodoPorto_2.set('fieldImages', {'Name': 'TextEdit', });
+lyr_Anexo02readeDocas_3.set('fieldImages', {'Name': 'TextEdit', });
+lyr_Anexo01reaPrincipaldoPorto_4.set('fieldImages', {'Name': 'TextEdit', });
+lyr_Anexo04reaAquaviria_1.set('fieldLabels', {'Name': 'inline label - visible with data', });
+lyr_Anexo03readeExpansodoPorto_2.set('fieldLabels', {'Name': 'inline label - visible with data', });
+lyr_Anexo02readeDocas_3.set('fieldLabels', {'Name': 'inline label - visible with data', });
+lyr_Anexo01reaPrincipaldoPorto_4.set('fieldLabels', {'Name': 'inline label - visible with data', });
+lyr_Anexo01reaPrincipaldoPorto_4.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
